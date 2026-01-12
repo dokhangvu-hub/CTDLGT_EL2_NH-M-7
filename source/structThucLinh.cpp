30692cfe8dd29fb05d4258168a2ba7de09b036d8
DuyThuan1205 <thuan21026@gmail.com>
2026-01-12 18:36:45 +0700

    Update structThucLinh.cpp

--- source/structThucLinh.cpp
+++ source/structThucLinh.cpp
@@ -1,19 +1,14 @@
-
 struct NhanVien {
-    string maNV;        // Mã nhân viên (không trùng)
-    string hoTen;       // Họ tên nhân viên
-    string ngaySinh;    // Ngày tháng năm sinh (dd/mm/yyyy)
-    string email;       // Email liên hệ
-    string diaChi;      // Địa chỉ liên lạc
-    string soDienThoai; // Số điện thoại
-    int soNgayCong;     // Số ngày công trong tháng
-    double luongNgay;   // Lương theo ngày
-    double thucLinh;    // Lương thực lĩnh
+    char maNV[20];        // Mã nhân viên
+    char hoTen[50];       // Họ và tên
+    int namSinh;          // Năm sinh
+    char chucVu[30];      // Chức vụ
+    float luongNgay;      // Lương 1 ngày
+    int soNgayCong;       // Số ngày làm việc trong tháng
+    float thucLinh;       // Lương thực lĩnh
 };
 struct Node {
-    NhanVien data;  // Dữ liệu nhân viên
-    Node* next;     // Con trỏ trỏ đến node tiếp theo
+    NhanVien data;   // Lưu thông tin 1 nhân viên
+    Node* next;      // Con trỏ trỏ đến node kế tiếp
 };
-void tinhThucLinh(NhanVien &nv) {
-    nv.thucLinh = nv.soNgayCong * nv.luongNgay; //Thực lĩnh = số ngày công * lương ngày
-}
+nv.thucLinh = nv.soNgayCong * nv.luongNgay;
